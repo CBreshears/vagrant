0d29b35f0d9842b45a8e0c22bb7b38ea8f66c461
Clay Breshears <43833429+CBreshears@users.noreply.github.com>
2018-11-12 16:49:23 -0800

    Debug/fix some problems with build script

--- build_genomicsdb.sh
+++ build_genomicsdb.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+CMAKE_BUILD_TYPE=Debug
+CMAKE_INSTALL_PREFIX=$HOME
+
+check_rc() {
+  if [[ $# -eq 1 ]]; then
+    if [[ -z $1 ]]; then
+      echo "make returned $1. Quitting GenomicsDB Build"
+      exit $1
+    fi
+  fi
+}
+
 source ~/env-protobuf-3.0.x.sh
 
 cd $HOME
+
+if [[ "$PROTOBUF_LIBRARY" = "" ]]; then
+  echo "PROTOBUF_LIBRARY env variable not defined. Exiting build."
+  exit 1
+else
+  echo "PROTOBUF_LIBRARY env variable set to $PROTOBUF_LIBRARY"
+fi
 
 # GenomicsDB
 echo 
@@ -13,13 +32,8 @@ git submodule update --recursive --init
 mkdir build
 cd build
 
-if [[ ! -z $PROTOBUF_LIBRARY ]]; then
-  echo "PROTOBUF_LIBRARY env variable not defined. Exiting build."
-  exit -1
-fi
-
 echo "Building GenomicsDB without Cloud Storage support"
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DBUILD_JAVA=1 -D${PROTOBUF_LIBRARY} ..
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DBUILD_JAVA=1 -DPROTOBUF_LIBRARY=${PROTOBUF_LIBRARY} ..
 
 check_rc $!
 make -j4 && make install
